4bf6d8580047459788d19d5e9bbbf70d02b6ff16
Vijay-Vaddi <vijayzvaddi@gmail.com>
2024-04-03 15:31:48 +0100

    styling

--- Django_todo_list/settings.py
+++ Django_todo_list/settings.py
@@ -114,12 +114,12 @@ USE_I18N = True
 USE_TZ = True
 
 LOGIN_URL = 'login'
-
+ 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'static_files'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
